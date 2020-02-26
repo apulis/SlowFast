25f068181b93383e16ff061240b855b0664683c8
Kevin Qi <qiwen@qiwen.name>
2020-02-26 23:04:08 +0800

    support hmdb51

---
+++ data_tools/hmdb51/generate_annotation.py
@@ -0,0 +1,34 @@
+import os, glob, json, pwd
+import os.path as osp
+
+def line2rec(line):
+    items = line.strip().split(' ')
+    path = items[0] + '.avi'
+    label = items[1]
+    return path, label
+
+def main():
+    '''
+    Assume the dataset's root is '${slowfast}/data/hmdb51'
+    '''
+    hmdb51_root = 'data/hmdb51'
+    train_lst_path = osp.join(os.getcwd(), '{}/hmdb51_train_split_1_videos.txt'.format(hmdb51_root))
+    val_lst_path = osp.join(os.getcwd(), '{}/hmdb51_val_split_1_videos.txt'.format(hmdb51_root))
+
+    with open(train_lst_path) as train_f:
+        # generate train.csv
+        with open(osp.join(hmdb51_root, "train.csv"), "w") as f:
+            for line in train_f.readlines():
+                path, label = line2rec(line)
+                f.write((osp.join(os.getcwd(), hmdb51_root + '/videos/' + path) + "," + label) + "\n")
+
+    with open(val_lst_path) as val_f:
+        # generate val.csv
+        with open(osp.join(hmdb51_root, "val.csv"), "w") as f:
+            for line in val_f.readlines():
+                path, label = line2rec(line)
+                f.write((osp.join(os.getcwd(), hmdb51_root + '/videos/' + path) + "," + label) + "\n")
+
+
+if __name__ == "__main__":
+    main()
