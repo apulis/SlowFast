e8c074543b8e009863262426d499dbf249427f33
Kevin <qiwen@qiwen.name>
2020-03-11 19:53:42 +0800

    Update logging.py

--- slowfast/utils/logging.py
+++ slowfast/utils/logging.py
@@ -24,17 +24,6 @@ def _suppress_print():
 
 def _get_time_str():
     return time.strftime('%Y%m%d_%H%M%S', time.localtime())
-
-def _add_file_handler(filename=None,
-                      mode='w',
-                      level=logging.INFO):
-    logger = get_logger(__name__)
-    file_handler = logging.FileHandler(filename, mode)
-    file_handler.setFormatter(
-        logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-    file_handler.setLevel(level)
-    logger.addHandler(file_handler)
-    return logger
     
 def setup_logging(workdir='.'):
     """
@@ -45,16 +34,18 @@ def setup_logging(workdir='.'):
     _FORMAT = "[%(levelname)s: %(filename)s: %(lineno)4d]: %(message)s"
 
     if du.is_master_proc():
+        filename = '{}.log'.format(_get_time_str())
+        log_file = os.path.join(workdir, filename)
+
         # Enable logging for the master process.
         logging.root.handlers = []
         logging.basicConfig(
-            level=logging.INFO, format=_FORMAT, stream=sys.stdout
+            level=logging.INFO, format=_FORMAT,
+            handlers=[
+                logging.FileHandler(log_file),
+                logging.StreamHandler()
+            ]
         )
-
-        log_dir = ''
-        filename = '{}.log'.format(_get_time_str())
-        log_file = os.path.join(log_dir, filename)
-        _add_file_handler(log_file, level=logging.INFO)
     else:
         # Suppress logging for non-master processes.
         _suppress_print()
